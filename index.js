f6ae48933454742bff2fb1d416742b40a3c160d4
yashas8197 <yashasjr7@gmail.com>
2024-09-13 16:41:22 +0530

    feat: Adding apis to get users and user by id

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const users = [
+  {
+    id: 1,
+    username: "octocat",
+    name: "The Octocat",
+    repoCount: 8,
+    location: "San Francisco",
+  },
+  {
+    id: 2,
+    username: "torvalds",
+    name: "Linus Torvalds",
+    repoCount: 25,
+    location: "Portland",
+  },
+  {
+    id: 3,
+    username: "gaearon",
+    name: "Dan Abramov",
+    repoCount: 50,
+    location: "London",
+  },
+  {
+    id: 4,
+    username: "addyosmani",
+    name: "Addy Osmani",
+    repoCount: 42,
+    location: "Mountain View",
+  },
+  {
+    id: 5,
+    username: "tj",
+    name: "TJ Holowaychuk",
+    repoCount: 150,
+    location: "Victoria",
+  },
+];
+
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+app.get("/users/:id", (req, res) => {
+  let userId = req.params.id;
+  let user = users.find((user) => user.id == userId);
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404).json({ message: "User not found" });
+  }
+});
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running in port 3000`);
+});
